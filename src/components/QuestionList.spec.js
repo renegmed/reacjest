9757939adc2982b427e59ce46cfc8a7c8dda22f4
ren medal <rmedal@pop-os.localdomain>
2019-11-29 12:37:18 -0500

    added before and after all, each

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,7 +1,27 @@
 import { isTSAnyKeyword } from "@babel/types"
 
 describe(`The question list`, () => {
+    beforeEach( () => {
+        console.log("Before each!")
+    });
+
+    beforeAll( () => {
+        console.log("Before all!")
+    });
+
+    afterEach( () => {
+        console.log("After each!")
+    });
+
+    afterAll( () => {
+        console.log("After all!")
+    });
+    
     it ("should display a list of items", ()=>{
+        expect(40 + 2).toEqual(42);
+    })
+
+    it ("should display before the meaning of life", ()=>{
         expect(40 + 2).toEqual(42);
     })
 })
